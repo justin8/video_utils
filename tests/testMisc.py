979746f9bb9d14872dbc2748d996d35f86cd7cca
Justin Dray <justin@dray.be>
2016-11-23 21:32:27 +1000

    Add isVideo tests

--- tests/testMisc.py
+++ tests/testMisc.py
@@ -23,3 +23,31 @@ class testMisc(unittest.TestCase):
                 ]
 
         self.assertEqual(misc.getVideosInFileList(test_data), expected_result)
+
+    def testIsVideo(self):
+        videoFilenames = ["test file 123x124 - name.avi",
+                           "test file 123x124 - name.mkv",
+                           "test file 123x124 - name.mp4",
+                           "test file 123x124 - name.mpg",
+                           "test file 123x124 - name.mpeg",
+                           "test file 123x124 - name.mov",
+                           "test file 123x124 - name.MOV",
+                           "test file 123x124 - name.m4v",
+                           "test file 123x124 - name.flv",
+                           "test file 123x124 - name.FLV",
+                           "test file 123x124 - name.divx",
+                           "test file 123x124 - name.ts",
+                           "test file 123x124 - name.wmv",
+        ]
+
+        nonVideoFilenames = ["test file 123x124 - name.foo",
+                               "test file 123x124 - name.bar",
+                               "test file 123x124 - name.txt",
+                               "test file 123x124 - name.jpg",
+        ]
+
+        for filename in videoFilenames:
+            self.assertTrue(misc.isVideo(filename))
+
+        for filename in nonVideoFilenames:
+            self.assertFalse(misc.isVideo(filename))
