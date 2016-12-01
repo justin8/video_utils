f1d2e56d6612541fe5e60a9c29d454504c11c0e7
Justin Dray <justin@dray.be>
2016-12-02 08:24:27 +1000

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="video_utils",
-    version="1.0.4",
+    version="1.0.5",
     author="Justin Dray",
     author_email="justin@dray.be",
     url="https://github.com/justin8/tv_report",
