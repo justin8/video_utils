0ebca4450d88f216f1d5a94e225f1aa7a51d68b7
Justin Dray <justin@dray.be>
2019-12-25 00:35:31 +1000

    Patch open correctly

--- tests/test_filemap_storage.py
+++ tests/test_filemap_storage.py
@@ -28,19 +28,41 @@ def test_storage_path(mock_md5, mock_makedirs, mock_expanduser, target):
 
 
 @patch("pickle.load", autospec=True)
-def test_storage_load(mock_load, target):
+@patch("os.path.exists", autospec=True, return_value=True)
+def test_storage_load_cache(mock_exists, mock_load, target):
     m = mock_open()
     mock_load.return_value = {"asdfg": 1234}
-    with patch("__main__.open", m):
+    with patch("builtins.open", m):
         result = target.load()
         assert result == {"asdfg": 1234}
 
 
 @patch("pickle.load", autospec=True)
-def test_storage_load_corrupt_file(mock_load, target):
+@patch("os.path.exists", autospec=True, return_value=False)
+def test_storage_load_no_file(mock_exists, mock_load, target):
+    m = mock_open()
+    mock_load.return_value = {"asdfg": 1234}
+    with patch("builtins.open", m):
+        result = target.load()
+        assert result == {}
+
+
+@patch("pickle.load", autospec=True)
+@patch("os.path.exists", autospec=True, return_value=True)
+def test_storage_load_no_cache(mock_exists, mock_load, target):
+    m = mock_open()
+    mock_load.return_value = {"asdfg": 1234}
+    with patch("builtins.open", m):
+        result = target.load(use_cache=False)
+        assert result == {}
+
+
+@patch("pickle.load", autospec=True)
+@patch("os.path.exists", autospec=True, return_value=True)
+def test_storage_load_corrupt_file(mock_exists, mock_load, target):
     m = mock_open()
     mock_load.side_effect = EOFError
-    with patch("__main__.open", m):
+    with patch("builtins.open", m):
         result = target.load()
         assert result == {}
 
