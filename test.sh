2a0b356aaf4fa075751f4b537817cab64804dd2a
Justin Dray <justin@dray.be>
2020-01-07 13:50:50 +1000

    Test out codecov

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-poetry run pytest --cov-report term-missing --cov-report html --cov video_utils tests/
+poetry run pytest --cov-report term-missing --cov-report xml --cov-report html --cov video_utils tests/
